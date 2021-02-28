8f186c2ca7b23a4277291f5f2a9f022f08015fd5
Matej Knopp <matej.knopp@gmail.com>
2021-02-28 23:07:25 +0100

    Try to load shader compiler
    Required for Windows 7

--- nanoshell/src/shell/platform/win32/init.rs
+++ nanoshell/src/shell/platform/win32/init.rs
@@ -9,6 +9,15 @@ use super::{
 
 pub fn init_platform(_context: Rc<Context>) -> PlatformResult<()> {
     unsafe {
+        // Angle will try opening these with GetModuleHandleEx, which means they need to be
+        // loaded first; Otherwise it falls back to d3dcompiler_47, which is not present on
+        // some Windows 7 installations.
+        if LoadLibraryW(utf16_lit::utf16_null!("d3dcompiler_47.dll").as_ptr()) == 0 {
+            if LoadLibraryW(utf16_lit::utf16_null!("d3dcompiler_46.dll").as_ptr()) == 0 {
+                LoadLibraryW(utf16_lit::utf16_null!("d3dcompiler_43.dll").as_ptr());
+            }
+        }
+
         OleInitialize(null_mut()).as_platform_result()?;
     }
     init_dxgi_hook();
